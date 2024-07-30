b10d7a76bfe8069d495cbd878c7f3e16d7fb2885
Novinity <novinity63@gmail.com>
2024-07-30 19:18:18 +1000

    1.0.0

---
+++ Patches/DivingBellSuitCellUIPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace BetterDivingBellUI.Patches {
+    [HarmonyPatch(typeof(DivingBellSuitCellUI))]
+    public static class DivingBellSuitCellUIPatch {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(DivingBellSuitCellUI.Set))]
+        public static void Set(Player player, float dst, DivingBellSuitCellUI __instance) {
+            if (player.data.dead) {
+                __instance.m_oxygenText.text = "DECEASED";
+                __instance.m_oxygenText.color = UnityEngine.Color.red;
+            }
+            else {
+                float oxyAmt = player.data.OxygenPercentage();
+                float greenAmt = oxyAmt;
+                float redAmt = 1f - greenAmt;
+                __instance.m_oxygenText.color = new Color(redAmt, greenAmt, 0);
+            }
+        }
+    }
+}
